29fb1b3eb6cfaf29a7c89a123eaf5bcce72028fd
iqbal <dhillon.iqbal.116@gmail.com>
2021-01-19 21:05:09 +0530

    section 4 routes notes in web file

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/admin', function () {
+    // return view('welcome');
+    return "admin is here";
 });
+
+
+Route::get('/about' , function(){
+
+        return "hi about page";
+});
+
+Route::get("contact",function(){
+
+        return "hi am contact";
+});
+
+//passing variables in routes
+
+Route::get('/posts/{id}/{name}' , function($id,$name){
+return "this is post no. $id for $name";
+});
+
+//nicknaming the routes
+
+Route::get('users/posts',array( 'as'=>'users.home' ,function(){
+
+    $url=route('users.home');
+    return $url;
+
+}));
+
