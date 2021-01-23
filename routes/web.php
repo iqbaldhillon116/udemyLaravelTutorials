6b5f74f32b014e883aab455aae5465ec02d006a5
iqbal <dhillon.iqbal.116@gmail.com>
2021-01-23 23:07:34 +0530

    section 9 Raw sql queries in routes

--- routes/web.php
+++ routes/web.php
@@ -94,10 +94,53 @@ Route::get('/posts/{id}/{name}','PostsResourceController@show_post');
 
 
 /*-------------------------------
-* section 7 : hot to use blade templating engine in laravel
+* section 7 : how to use blade templating engine in laravel
 -------------------------------
 */
 
 Route::get('/rollno','PostsResourceController@roll_no');
 
 Route::get('/class/{id}','PostsResourceController@class');
+
+/*-------------------------------
+* section 9 : Raw Sql Queries
+-------------------------------
+*/
+
+use Illuminate\Support\Facades\DB;
+
+Route::get('/insert',function(){
+
+    DB::insert("insert into posts(title,content,is_admin) values(?,?,?)" ,['learning laravel', 'Laravel is Awesome',1]);
+    echo "one row inserted";    
+});
+
+
+Route::get('/read', function(){
+
+   $result = DB::select('select * from posts where id=?' , [1]);
+
+//    print_r($result);
+//    echo "<br>";
+   foreach($result as  $value){
+
+    print_r($value->title);
+
+   }
+
+});
+
+
+Route::get('/update',function(){
+
+    $updated=DB::update("update posts set title='updated title' where id=?",[1]);
+
+    return $updated;
+});
+
+Route::get('/delete' , function(){
+
+    $deleted = DB::delete('delete from posts where id=?',[1]);
+
+    return $deleted;
+});
